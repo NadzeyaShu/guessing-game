f10ee968c1b0b83eab2fd27212c30281fb06bc1f
NadzeyaShu <chunta89@gmail.com>
2022-08-22 12:24:44 +0400

    impl

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,23 @@
 class GuessingGame {
-    constructor() {}
+
+    constructor() {
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.guessNumber = Math.round((this.max + this.min) / 2);
     }
 
     lower() {
-
+        this.max = this.guessNumber;
     }
 
     greater() {
-
+        this.min = this.guessNumber;
     }
 }
 
